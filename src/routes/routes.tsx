10652c93ade9dd525c875b601029ad228d2d8371
Josenilsonfariasx <fariaslwork@gmail.com>
2024-05-22 04:24:39 -0300

    feat: adding routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom"
 import { UserProvider } from "../providers/UserContext"
 import Register from "../pages/Register/Register"
+import Login from "../pages/Login/Login"
 
 export const RoutesMain = () => {
     return (
         <UserProvider>
             <Routes>
-                {/* <Route path="/" element={<SingIn />} /> */}
+                <Route path="/" element={<Login />} />
                 <Route path="/register" element={<Register /> } />
                 {/* <Route path="/home" element={<ProtectedRoutes />}>
                     <Route index element={ <TechProvider><Dashboard /></TechProvider>} />
