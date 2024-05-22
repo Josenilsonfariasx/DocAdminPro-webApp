a80dc905552ca2dcc7e3e185be07995bc8dc09bf
Josenilsonfariasx <fariaslwork@gmail.com>
2024-05-22 04:23:51 -0300

    config: libs config

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import { ToastContainer } from 'react-toastify'
+import "react-toastify/dist/ReactToastify.css"
 import './App.css'
+import { RoutesMain } from './routes/routes'
 
 function App() {
 
   return (
     <>
+      <RoutesMain />
+      <ToastContainer
+        position="top-center"
+        autoClose={4000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+        />
     </>
   )
 }
