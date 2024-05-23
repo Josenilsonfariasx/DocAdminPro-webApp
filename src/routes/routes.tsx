609ef88f88972065bddf4d1efd1e40c94133f665
Josenilsonfariasx <fariaslwork@gmail.com>
2024-05-23 07:36:33 -0300

    feat: adding routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom"
 import { UserProvider } from "../providers/UserContext"
 import Register from "../pages/Register/Register"
 import Login from "../pages/Login/Login"
+import { ProtectedRoutes } from "../components/ProtectedRoutes/ProtectedRoutes"
+import {Dashboard} from "../pages/Dashboard/Dashboard"
 
 export const RoutesMain = () => {
     return (
@@ -9,9 +11,9 @@ export const RoutesMain = () => {
             <Routes>
                 <Route path="/" element={<Login />} />
                 <Route path="/register" element={<Register /> } />
-                {/* <Route path="/home" element={<ProtectedRoutes />}>
-                    <Route index element={ <TechProvider><Dashboard /></TechProvider>} />
-                </Route> */}
+                <Route path="/home" element={<ProtectedRoutes />}>
+                    <Route index element={ <Dashboard />} />
+                </Route>
             </Routes>
         </UserProvider>
     )
