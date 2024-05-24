670ee8cf0339b908150f4e6693bcd76e2023f7cc
Josenilsonfariasx <fariaslwork@gmail.com>
2024-05-24 06:10:23 -0300

    feat: adding routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -4,6 +4,12 @@ import Register from "../pages/Register/Register"
 import Login from "../pages/Login/Login"
 import { ProtectedRoutes } from "../components/ProtectedRoutes/ProtectedRoutes"
 import {Dashboard} from "../pages/Dashboard/Dashboard"
+import { Documents } from "../pages/Documents/Documents"
+import UserProfileForm from "../pages/Pefil/Profile"
+import { Search } from "../pages/Search/Search"
+import Forgot from "../pages/ForgotPassword/ForgotPassword"
+import ConfirmCode from "../pages/ConfirmCode/ConfirmCode"
+import ResetPassword from "../pages/ResetPassword/ResetPassword"
 
 export const RoutesMain = () => {
     return (
@@ -11,8 +17,14 @@ export const RoutesMain = () => {
             <Routes>
                 <Route path="/" element={<Login />} />
                 <Route path="/register" element={<Register /> } />
-                <Route path="/home" element={<ProtectedRoutes />}>
-                    <Route index element={ <Dashboard />} />
+                <Route path="/forgot" element={< Forgot />} />
+                <Route path="/code" element={< ConfirmCode />} />
+                <Route path="/password" element={< ResetPassword />} />
+                <Route element={<ProtectedRoutes />}>
+                    <Route path="/home" element={ <Dashboard />} />
+                    <Route  path="/docs" element={ <Documents />} />
+                    <Route path="/profile" element={< UserProfileForm />} />
+                    <Route path="/filter" element={< Search />} />
                 </Route>
             </Routes>
         </UserProvider>
